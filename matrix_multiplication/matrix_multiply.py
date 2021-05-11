95caec9e4b7726addf8dbf1b38f87fa5da7eced6
raghav <rgv3300@gmail.com>
2021-05-11 09:18:26 -0400

    matrix multiplication algorithm

---
+++ matrix_multiplication/matrix_multiply.py
@@ -0,0 +1,12 @@
+def matrix_multiply(a,b,c):
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            for k in range(len(b)):
+                c[i][j] += a[i][k] * b[k][j]
+    print(c)
+
+
+a = [[1,2,3],[3,4,5]]
+b= [[4,5,6],[6,5,4]]
+c = [[0,0,0],[0,0,0]]
+matrix_multiply(a,b,c)
