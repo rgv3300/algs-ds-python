43d202280d3d7ce411f450a5edcfd969d6957828
raghav <rgv3300@gmail.com>
2021-05-14 09:22:29 -0400

    searching and adding items to linked list

--- linked_list/linkedList.c
+++ linked_list/linkedList.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
+typedef struct Node Node;
+struct Node {
+	int data;
+	Node* next;
+};
+
+void printNode(Node* n) {
+	while(n != NULL) {
+		printf(" % d \n ", n->data);
+		n = n->next;
+	}
+}
+
+Node* searchItem(Node* item, int x) {
+	if (item == NULL) return (NULL);
+
+	if (item->data == x)
+	       	return (item); 
+	else
+		return (searchItem(item->next, x));
+}
+
+void addItem(Node** item, int x) {
+	Node* p;
+
+	p = malloc(sizeof(Node));
+	p->data = x;
+	p->next = *item;
+	*item = p;
+}	
 
 int main() {
-	struct Node {
-		int data;
-		struct Node* next;
-	}
-
-	struct Node* head = NULL;
-	struct Node* second = NULL;
-	struct Node* third = NULL;
+	 Node* head = NULL;
+	 Node* second = NULL;
+	 Node* third = NULL;
 
 	head = (struct Node*)malloc(sizeof(struct Node));
 	second = (struct Node*)malloc(sizeof(struct Node));
@@ -20,4 +45,11 @@ int main() {
 	second->next = third;
 	third->data = 3;
 	third->next = NULL;
+	
+	printNode(head);
+
+	Node** temp = &head;
+	
+	addItem(temp, 5);	
+	printNode(head);
 }
