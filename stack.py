a7ad6de428aa4f7cd8f5ecb3515215dfeb85aa9d
raghav <rgv3300@gmail.com>
2021-05-16 19:53:08 -0400

    stack changes

--- stack.py
+++ stack.py
@@ -1,28 +1,29 @@
 class Node:
-    def _init_(self, data, next):
-        self.data = None
+    def __init__(self, data=None):
+        self.data = data
         self.next = None
 
 class Stack:
-    def _init_(self):
-        self.head = None
+    def __init__(self):
+        self.data = None
     def push(self,item):
-        if self.head == None:
-            self.head = Node(item)
+        if self.data == None:
+            self.data = Node(item)
         else:
             new = Node(item)
-            new.next = self.head
-            self.head = new
+            new.next = self.data
+            self.data = new
     def pop(self):
-        popped = self.head
-        self.head = self.head.next
+        popped = self.data
+        self.data = self.data.next
         popped.next = None
         return popped.data
 
 a = Stack()
 a.push(5)
-print(a)
+print(a.data.data)
 a.push(6)
+print(a.data.next.data)
 a.push(7)
-print(a)
+print(a.data.next.data)
 a.pop()
