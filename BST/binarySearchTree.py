46848c6419e8107fac10ed09250592b2a9081c43
raghav <rgv3300@gmail.com>
2021-05-19 08:27:00 -0400

    min max and search

--- BST/binarySearchTree.py
+++ BST/binarySearchTree.py
@@ -4,6 +4,16 @@ class node:
         self.left = None
         self.right = None
 
+def lookupTree(rootNode,item):
+    if rootNode == None:
+        return
+    if rootNode.data == item:
+        return True
+    if rootNode.data > item:
+        return lookupTree(rootNode.left, item)
+    else:
+        return lookupTree(rootNode.right, item)
+
 def insertNode(rootNode,item):
     temp = node(item)
     if rootNode == None:
@@ -19,7 +29,31 @@ def insertNode(rootNode,item):
             return
         insertNode(rootNode.right, item)
 
+def searchMinimum(rootNode):
+    min = rootNode
+    if rootNode == None:
+        return
+    while min.left != None:
+        min = min.left
+    return min.data
+
+def searchMaximum(rootNode):
+    max = rootNode
+    if rootNode == None:
+        return
+    while max.right != None:
+        max = max.right
+    return max.data
+
+
 #must define rootNode first
 a = node(3)
 #automatically sort nodes after rootNode is inserted
 insertNode(a,2)
+insertNode(a,1)
+insertNode(a,7)
+insertNode(a,4)
+
+
+b = lookupTree(a,8)
+print(b)
